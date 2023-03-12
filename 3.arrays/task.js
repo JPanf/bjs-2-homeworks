34456b13517bf632c79829639dd59238c81f8c14
Julia <julia.panfilova@mail.ru>
2023-03-12 19:23:32 +0300

    task 4.OBJECTS done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,13 @@
 function compareArrays(arr1, arr2) {
-    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);  
+	return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
 
 
 function getUsersNamesInAgeRange(users, gender) {
-    let filterByGender = users.filter(user => user.gender === gender);
-    let averAge = filterByGender.reduce((init, user) => init + user.age, 0) / filterByGender.length;
-    if (isNaN(averAge)) {
-        return 0;
-    }
-    return averAge;
+	let filterByGender = users.filter(user => user.gender === gender);
+	let averAge = filterByGender.reduce((init, user) => init + user.age, 0) / filterByGender.length;
+	/*if (isNaN(averAge)) {
+		return 0;
+	}*/
+	return averAge || 0;
 }
